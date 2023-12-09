52c16bbb62034ab9d9c1b2abbd80c6dd7b7e5bf9
Tom Warner <tomwamt@gmail.com>
2023-12-09 11:29:49 -0700

    Day 9 dedup

--- src/day9.rs
+++ src/day9.rs
@@ -5,14 +5,18 @@ pub struct Solution;
 impl crate::Solution for Solution {
     fn solve_1(&self, input: String) -> String {
         parse_lines_with(&input, parse_value_seq)
-            .map(predict_next_value)
+            .map(predict_value)
             .sum::<i32>()
             .to_string()
     }
 
     fn solve_2(&self, input: String) -> String {
         parse_lines_with(&input, parse_value_seq)
-            .map(predict_prev_value)
+            .map(|mut it| {
+                it.reverse();
+                it
+            })
+            .map(predict_value)
             .sum::<i32>()
             .to_string()
     }
@@ -24,20 +28,11 @@ fn parse_value_seq(line: &str) -> Vec<i32> {
         .collect()
 }
 
-fn predict_next_value(seq: Vec<i32>) -> i32 {
+fn predict_value(seq: Vec<i32>) -> i32 {
     if seq.iter().copied().all(|v| v == 0) {
         return 0;
     }
     let diffs = seq.windows(2).map(|w| w[1] - w[0]).collect();
-    let next_diff = predict_next_value(diffs);
-    seq[seq.len() - 1] + next_diff
-}
-
-fn predict_prev_value(seq: Vec<i32>) -> i32 {
-    if seq.iter().copied().all(|v| v == 0) {
-        return 0;
-    }
-    let diffs = seq.windows(2).map(|w| w[1] - w[0]).collect();
-    let prev_diff = predict_prev_value(diffs);
-    seq[0] - prev_diff
+    let diff = predict_value(diffs);
+    seq[seq.len() - 1] + diff
 }
